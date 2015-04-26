6efd1abb46f209acbb13216c193ce9e33fa556d4
nucular <nucular@users.noreply.github.com>
2015-04-26 19:03:30 +0200

    Basic project structure

---
+++ Window1.xaml.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * Date: 26.04.2015
+ * Time: 19:01
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace XonWin
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+	}
+}
